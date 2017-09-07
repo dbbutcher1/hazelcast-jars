9d2a9bcac55b8fd0abc3c3195071a1c38ede4f95
David Butcher <dbutcher@coxautoinc.com>
2017-09-07 14:05:10 -0400

    Bug fix for when newline character is included in require line

--- lib/hazelcast/jars.rb
+++ lib/hazelcast/jars.rb
@@ -4,20 +4,22 @@ module Hazelcast
   class Jars
     class << self
 
+      # Adding .gsub( "\n", '' ) to remove newline character due to possible bug
+
       def client
-        require "hazelcast/jars/hazelcast-client-#{version}.jar"
+        require "hazelcast/jars/hazelcast-client-#{version}.jar".gsub( "\n", '' )
       end
 
       def server
-        require "hazelcast/jars/hazelcast-#{version}.jar"
+        require "hazelcast/jars/hazelcast-#{version}.jar".gsub( "\n", '' )
       end
 
       def all
-        require "hazelcast/jars/hazelcast-all-#{version}.jar"
+        require "hazelcast/jars/hazelcast-all-#{version}.jar".gsub( "\n", '' )
       end
 
       def spring
-        require "hazelcast/jars/hazelcast-spring-#{version}.jar"
+        require "hazelcast/jars/hazelcast-spring-#{version}.jar".gsub( "\n", '' )
       end
 
     end
